a11544b3f21c003ff6fe10910e189030226d9258
Erdemozer01 <ozer246@gmail.com>
2025-03-29 17:29:46 +0300

    ..

--- lesson1.py
+++ lesson1.py
@@ -1,12 +1,10 @@
 from time import sleep
 import gpiozero
+from signal import pause
+
 
 led = gpiozero.LED(17)
 
-while True:
-    led.value = 0  # off
-    sleep(1)
-    led.value = 0.5  # half brightness
-    sleep(1)
-    led.value = 1  # full brightness
-    sleep(1)
+led.blink()
+
+pause()
