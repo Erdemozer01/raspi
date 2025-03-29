9fb74ec47be6cf7d94f2f6f4c6315a04580d32b6
Erdemozer01 <ozer246@gmail.com>
2025-03-29 18:44:49 +0300

    ..

--- lesson1.py
+++ lesson1.py
@@ -2,11 +2,16 @@ import gpiozero
 from signal import pause
 
 
-led = gpiozero.PWMLED(17)
-try:
-    led.pulse()
-    pause()
-except KeyboardInterrupt:
-    print("Bye")
+def led(pin_number=None):
 
 
+    led = gpiozero.PWMLED(pin_number)
+
+    try:
+        led.pulse()
+        pause()
+    except KeyboardInterrupt:
+        print("Bye")
+
+
+led(17)
