ef1495fe99d4613055b75d2b2577ceab0e41ebc7
Erdemozer01 <ozer246@gmail.com>
2025-03-30 00:35:25 +0300

    app created

--- led_app/lamba.py
+++ led_app/lamba.py
@@ -1,8 +1,20 @@
-from gpiozero import LEDBoard
+from gpiozero import TrafficLights
+from time import sleep
 from signal import pause
 
-leds = LEDBoard( 13, 19, 26, pwm=True)
+lights = TrafficLights(2, 3, 4)
 
-leds.value = (0.6, 0.8, 1.0)
+def traffic_light_sequence():
+    while True:
+        yield (0, 0, 1) # green
+        sleep(10)
+        yield (0, 1, 0) # amber
+        sleep(1)
+        yield (1, 0, 0) # red
+        sleep(10)
+        yield (1, 1, 0) # red+amber
+        sleep(1)
+
+lights.source = traffic_light_sequence()
 
 pause()
