980e3b543165c8242cb7621e8b621437af5b345b
Erdemozer01 <ozer246@gmail.com>
2025-03-29 14:07:34 +0300

    ..

--- lesson1.py
+++ lesson1.py
@@ -1,15 +1,11 @@
 import time
-
-from gpiozero import LED
 import gpiozero
 
-led = LED(17)
+led = gpiozero.LED(17)
 
 while True:
     led.on()
     time.sleep(1)
     led.off()
     time.sleep(1)
-
-
-
+    gpiozero.Device.close()
