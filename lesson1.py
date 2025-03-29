7550b890a706ae0011b92971d8569e8e0ddf195f
Erdemozer01 <ozer246@gmail.com>
2025-03-29 14:17:34 +0300

    ..

--- lesson1.py
+++ lesson1.py
@@ -2,13 +2,10 @@ import time
 import gpiozero
 from signal import pause
 
-
-try:
-    led = gpiozero.LED(17)
-    while True:
-        led.on()
-        time.sleep(1)
-        led.off()
-        time.sleep(1)
-except:
+led = gpiozero.LED(17)
+while True:
+    led.on()
+    time.sleep(1)
+    led.off()
+    time.sleep(1)
     pause()
