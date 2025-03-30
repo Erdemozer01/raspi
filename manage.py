9069c47d4b1b749ece840118c67c53ec100900b5
Erdemozer01 <ozer246@gmail.com>
2025-03-30 19:45:25 +0300

    app created

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 from led_apps import led
 
-title = "*"*50 + "\n" + "İşlem Seçiniz" + "\n" + "1 - Pulse Kontrol" + "\n" + "2 - Traffic Lights"+ "\n" + "*"*50 + "\n\n"
+title = "*"*50 + "\n" + "Uygulamalar" + "\n" + "1 - Pulse Kontrol" + "\n" + "2 - Traffic Lights"+ "\n" + "*"*50 + "\n\n"
 
 
 while True:
 
-    choice_app = int(input(title))
+    choice_app = int(input("İşlem : "))
 
     if choice_app == 1:
 
@@ -15,4 +15,14 @@ while True:
             led.led_pulse(pin_number=pin_number)
         except:
             print("Pin numarasını doğru yazdığınızdan emin olun")
-            continue
+            pass
+    elif choice_app == 2:
+        try:
+            red = int(input("Kırmızı ısık pin: "))
+            yellow = int(input("Sarı ısık pin: "))
+            green = int(input("Yeşil ısık pin: "))
+
+            led.traffic_light(red_pin_number=red, green_pin_number=green, yellow_pin_number=yellow)
+        except:
+            print("Pin numarasını doğru yazdığınızdan emin olun")
+            pass
